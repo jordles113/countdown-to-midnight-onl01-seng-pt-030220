f62a27425ab6b09115d0253abb83bbc8439e94c1
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-06 02:45:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown (number)
-
- while number > 0 
- puts "#{number} SECOND(s)!"
+while number > 0 
+ puts "#{number} SECOND(S)!"
  number -= 1
  end 
  puts "HAPPY NEW YEAR!"
 end
+7
